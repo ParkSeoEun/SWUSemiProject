2b27a64f225687bea24ce3afdeef9876caa843b8
ParkSeoEun <silion10@naver.com>
2019-07-03 16:55:20 +0900

    세미 프로젝트 틀 1

---
+++ app/src/main/java/com/example/swusemiproject/MemoWriteActivity.java
@@ -0,0 +1,14 @@
+package com.example.swusemiproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MemoWriteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_memo_write);
+    }
+}
